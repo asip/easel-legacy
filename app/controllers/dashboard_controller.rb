aabb4d1f3027502af7e5576d0c8d92165555b272
Yasumasa Ashida <ys.ashida@gmail.com>
2017-11-05 10:20:34 +0900

    [add] add tag search

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class DashboardController < ApplicationController
+  before_action :query_params, only: [:show]
+
   def show
-    @frames = Frame.page(params[:page])
+    if @word.present?
+      @frames = Frame.tagged_with(@word, any: true).page(params[:page])
+    else
+      @frames = Frame.page(params[:page])
+    end
+  end
+
+  def query_params
+    @word = params[:word]
   end
 end
