90b30e02c78ecb0df9548f1dd3aedbc069918abd
Yasumasa Ashida <ys.ashida@gmail.com>
2017-11-05 15:26:24 +0900

    [update] update acts_as_taggable_on (4.0→5.0)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102231430) do
+ActiveRecord::Schema.define(version: 20171105061629) do
 
   create_table "frames", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
