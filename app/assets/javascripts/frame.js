f34b7a101400c673bbdf43f2ef573d8f2e6ad808
Yasumasa Ashida <ys.ashida@gmail.com>
2017-11-15 14:45:07 +0900

    [update] update frame.js

--- app/assets/javascripts/frame.js
+++ app/assets/javascripts/frame.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on('turbolinks:load', function() {
+document.addEventListener('turbolinks:load', function() {
   $('#tag_list').tagEditor({
     delimiter: ', '
   });
