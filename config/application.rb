079a40c2270400514ec4a5c424b9121c0337976e
Yasumasa Ashida <ys.ashida@gmail.com>
2017-11-29 15:23:16 +0900

    [update] upgrade  to 5.2.0.beta2

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-#require 'active_storage'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,14 +9,13 @@ Bundler.require(*Rails.groups)
 module Easel
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
     # generate .js instead of .coffee
     config.generators.javascript_engine = :js
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#show'
 
   get '/frames/' => 'frames#index', as: :frames
