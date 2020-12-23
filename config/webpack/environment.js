b91897d36ac3c729e72241abbe706bd18ca7a9a6
芦田健将 <asip@edith-mbp.local>
2020-12-24 00:27:27 +0900

    [update] update webpacker.js to 5.2.1

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
@@ -6,4 +7,12 @@ environment.splitChunks()
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+//const babelLoader= environment.loaders.get("babel")
+//babelLoader.options
+environment.loaders.delete('nodeModules')
+
+environment.loaders.prepend('erb', erb)
 module.exports = environment
