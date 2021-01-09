deb30a5fc18c8e35f89e4d136447ee2711fd6381
Yasumasa Ashida <ys.ashida@gmail.com>
2021-01-09 13:57:00 +0900

    [update] feat: check file extension in image_preview.js

--- app/javascript/packs/image_preview.js
+++ app/javascript/packs/image_preview.js
@@ -7,24 +7,28 @@ document.addEventListener('turbolinks:load', () => {
   const elm_upload = document.querySelector('#frame_image');
 
   elm_upload.addEventListener('change', function () {
-    // .file_filedからデータを取得して変数fileに代入します
-    var file = this.files[0];
-    // FileReaderオブジェクトを作成します
-    var reader = new FileReader();
-    // DataURIScheme文字列を取得します
-    reader.readAsDataURL(file);
-    // 読み込みが完了したら処理が実行されます
-    reader.onload = function () {
-      // 読み込んだファイルの内容を取得して変数imageに代入します
-      var image = this.result;
-      //console.log(pre_image.style.display);
-      pre_image.src = image;
-      // プレビュー画像がなければ表示します
-      if (pre_image.style.display == 'none') {
-        pre_image.style.display = 'block';
-        pre_content.append(document.createElement('br'));
-        pre_content.append(document.createElement('br'));
+    var file_name = this.value;
+    var file_ext = file_name.replace(/^.*\./, '').toLowerCase();
+    if (file_ext.match(/^(jpeg|jpg|png|gif)$/)) {
+      // .file_filedからデータを取得して変数fileに代入します
+      var file = this.files[0];
+      // FileReaderオブジェクトを作成します
+      var reader = new FileReader();
+      // DataURIScheme文字列を取得します
+      reader.readAsDataURL(file);
+      // 読み込みが完了したら処理が実行されます
+      reader.onload = function () {
+        // 読み込んだファイルの内容を取得して変数imageに代入します
+        var image = this.result;
+        //console.log(pre_image.style.display);
+        pre_image.src = image;
+        // プレビュー画像がなければ表示します
+        if (pre_image.style.display == 'none') {
+          pre_image.style.display = 'block';
+        }
       }
+    } else {
+      pre_image.style.display = 'none';
     }
   });
 });
