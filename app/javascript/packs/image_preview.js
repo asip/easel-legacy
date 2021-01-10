e84000e41a9cd3626985bbc4a9005bdc7f9a3b38
Yasumasa Ashida <ys.ashida@gmail.com>
2021-01-10 20:43:21 +0900

    [update] fix: image_preview.js

--- app/javascript/packs/image_preview.js
+++ app/javascript/packs/image_preview.js
@@ -1,34 +1,36 @@
 
 document.addEventListener('turbolinks:load', () => {
 
-  const pre_content = document.querySelector('.prev-content');
+  //const pre_content = document.querySelector('.prev-content');
   const pre_image = document.querySelector('.prev-image');
 
   const elm_upload = document.querySelector('#frame_image');
 
-  elm_upload.addEventListener('change', function () {
-    var file_name = this.value;
-    var file_ext = file_name.replace(/^.*\./, '').toLowerCase();
-    if (file_ext.match(/^(jpeg|jpg|png|gif)$/)) {
-      // .file_filedからデータを取得して変数fileに代入します
-      var file = this.files[0];
-      // FileReaderオブジェクトを作成します
-      var reader = new FileReader();
-      // DataURIScheme文字列を取得します
-      reader.readAsDataURL(file);
-      // 読み込みが完了したら処理が実行されます
-      reader.onload = function () {
-        // 読み込んだファイルの内容を取得して変数imageに代入します
-        var image = this.result;
-        //console.log(pre_image.style.display);
-        pre_image.src = image;
-        // プレビュー画像がなければ表示します
-        if (pre_image.style.display == 'none') {
-          pre_image.style.display = 'block';
+  if (elm_upload) {
+    elm_upload.addEventListener('change', function () {
+      var file_name = this.value;
+      var file_ext = file_name.replace(/^.*\./, '').toLowerCase();
+      if (file_ext.match(/^(jpeg|jpg|png|gif)$/)) {
+        // .file_filedからデータを取得して変数fileに代入します
+        var file = this.files[0];
+        // FileReaderオブジェクトを作成します
+        var reader = new FileReader();
+        // DataURIScheme文字列を取得します
+        reader.readAsDataURL(file);
+        // 読み込みが完了したら処理が実行されます
+        reader.onload = function () {
+          // 読み込んだファイルの内容を取得して変数imageに代入します
+          var image = this.result;
+          //console.log(pre_image.style.display);
+          pre_image.src = image;
+          // プレビュー画像がなければ表示します
+          if (pre_image.style.display == 'none') {
+            pre_image.style.display = 'block';
+          }
         }
+      } else {
+        pre_image.style.display = 'none';
       }
-    } else {
-      pre_image.style.display = 'none';
-    }
-  });
+    });
+  }
 });
