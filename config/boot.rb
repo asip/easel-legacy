56ef71e26fb8068d18c3bcc372e018510f339086
Yasumasa Ashida <ys.ashida@gmail.com>
2021-02-20 09:29:31 +0900

    refactor: for gems.rb and Gemfile

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,8 @@
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../gems.rb', __dir__ ) unless ENV['BUNDLE_GEMFILE'] && File.exist?(ENV['BUNDLE_GEMFILE']) 
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', __dir__) unless File.exist?(ENV['BUNDLE_GEMFILE'])
+unless ENV['BUNDLE_GEMFILE'] && File.exist?(ENV['BUNDLE_GEMFILE'])
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('../gems.rb', __dir__ )
+end
+unless File.exist?(ENV['BUNDLE_GEMFILE'])
+  ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', __dir__)
+end
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
