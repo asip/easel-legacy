de022f2282921f3aa172c7cf0cb48879038b0bc2
Yasumasa Ashida <ys.ashida@gmail.com>
2021-02-20 09:12:07 +0900

    fix: for gems.rb and Gemfile

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../gems.rb', __dir__)
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../gems.rb', __dir__ ) unless ENV['BUNDLE_GEMFILE'] && File.exist?(ENV['BUNDLE_GEMFILE']) 
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', __dir__) unless File.exist?(ENV['BUNDLE_GEMFILE'])
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
