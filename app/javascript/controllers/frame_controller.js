d555df92e6af4928c25ce7df4a5eca3f60288375
Yasumasa Ashida <ys.ashida@gmail.com>
2021-03-03 13:47:21 +0900

    refactor(js): refactoring of frame_controller

--- app/javascript/controllers/frame_controller.js
+++ app/javascript/controllers/frame_controller.js
@@ -2,34 +2,32 @@ import { Controller } from "stimulus"
 import Tagify from '@yaireo/tagify'
 
 export default class FrameController extends Controller {
-  static targets = ['lm', 'te', 'tl']
-  //static lm_elms = {};
-  lm_trigger = null;
-  tl_trigger = null;
+  static targets = ['lm', 'te', 'tl'];
   image;
-  editor;
+  tag_list;
+  tag_editor;
 
   connect() {
-    //this.lm_trigger = null;
+    var lm_trigger = null;
     try{
-      this.lm_trigger = this.lmTarget;
+      lm_trigger = this.lmTarget;
     } catch(e) {}
     var te_trigger = null;
     try{
       te_trigger = this.teTarget;
     } catch(e) {}
-    //this.tl_trigger = null;
+    //this.tag_list = null;
     try{
-      this.tl_trigger = this.tlTarget;
+      this.tag_list = this.tlTarget;
     } catch(e) {}
 
-    if(this.lm_trigger){
+    if(lm_trigger){
       const options = { showCloseButton: true };
-      this.image = new Luminous(this.lm_trigger, options);
+      this.image = new Luminous(lm_trigger, options);
     }
 
     if(te_trigger){
-      this.editor = new Tagify(te_trigger, {
+      this.tag_editor = new Tagify(te_trigger, {
         maxTags: 5,
         dropdown: {
           classname: "color-blue",
@@ -40,19 +38,19 @@ export default class FrameController extends Controller {
         }
       });
 
-      this.editor.on('add', e => this.saveTagList(e.detail.tagify));
-      this.editor.on('remove', e => this.saveTagList(e.detail.tagify));
+      this.tag_editor.on('add', e => this.saveTagList(e.detail.tagify));
+      this.tag_editor.on('remove', e => this.saveTagList(e.detail.tagify));
 
-      const tag_list = this.tl_trigger.value;
-      this.editor.removeAllTags();
-      if (tag_list.length > 0) {
-        this.editor.addTags(tag_list.split(','));
+      const tags = this.tag_list.value;
+      this.tag_editor.removeAllTags();
+      if (tags.length > 0) {
+        this.tag_editor.addTags(tags.split(","));
       }
     }
   }
 
   saveTagList(tagify) {
-    this.tl_trigger.value = this.editor.value.map(v => v.value).join(",");
+    this.tag_list.value = this.tag_editor.value.map(v => v.value).join(",");
     //console.log(this.tlTarget.value);
   }
 
@@ -63,10 +61,10 @@ export default class FrameController extends Controller {
       Array.from(image_elements).forEach(e => e.remove());
     }
 
-    if(this.editor){
-      this.editor.removeAllTags()
-      const editor_elements = this.element.getElementsByClassName('tagify');
-      Array.from(editor_elements).forEach(e => e.remove());
+    if(this.tag_editor){
+      this.tag_editor.removeAllTags()
+      const tag_editor_elements = this.element.getElementsByClassName('tagify');
+      Array.from(tag_editor_elements).forEach(e => e.remove());
     }
   }
 }
