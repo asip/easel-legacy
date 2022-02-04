8717991545fa1f2c64d1ef2e3657e6f3b385b940
Yasumasa Ashida <ys.ashida@gmail.com>
2022-02-05 07:51:40 +0900

    refactor: refactoring of frames controller

--- app/controllers/frames_controller.rb
+++ app/controllers/frames_controller.rb
@@ -8,21 +8,10 @@ class FramesController < ApplicationController
     @frames = Frame.where(nil)
 
     if @word.present?
-      # @frames = @frames.ransack({ name_or_tags_name_cont_or_users_name: @word}).result(distinct: true)
       @frames = @frames.joins(:tags, :user)
         .merge(ActsAsTaggableOn::Tag.where("tags.name like ?", "%#{@word}%"))
         .or(Frame.where("frames.name like ?", "%#{@word}%"))
         .or(User.where(name: @word))
-      # #@frames = Frame.includes(:tags, :user)
-      ##              .where(tags: {name: @word})
-      ##              .or(Frame.where('frames.name like ?', "%#{@word}%"))
-      ##              .or(User.where(name: @word))
-      ##              .distinct
-      # @frames = Frame.includes(:tags, :user).references(:tags, :user)
-      #            .where('tags.name like ?', "%#{@word}%")
-      #            .or(Frame.where('frames.name like ?', "%#{@word}%"))
-      #            .or(User.where(name: @word))
-
     end
 
     @frames = @frames.page(@page)
@@ -67,9 +56,9 @@ class FramesController < ApplicationController
   private
 
   def set_query
-    query_params = params_query
-    @word = query_params[:q]
-    @page = query_params[:page]
+    params_query = query_params
+    @word = params_query[:q]
+    @page = params_query[:page]
   end
 
   def set_frame
@@ -97,7 +86,7 @@ class FramesController < ApplicationController
     end
   end
 
-  def params_query
+  def query_params
     params.permit(
       :q,
       :page
