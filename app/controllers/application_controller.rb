0c4faea6877250bcf30104044cbb5d495fbb9c9e
Yasumasa Ashida <ys.ashida@gmail.com>
2022-02-18 13:03:51 +0900

    chore: sorcery-jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :require_login
+  before_action :token_confirmation
 
   def query_params
     params.permit(
@@ -15,5 +16,13 @@ class ApplicationController < ActionController::Base
 
   def not_authenticated
     redirect_to root_path
+  end
+
+  def token_confirmation
+    return if current_user.blank?
+    if current_user && current_user.token_expire?
+      current_user.reset_token
+      logout
+    end
   end
 end
