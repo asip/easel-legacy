1d99018fa78fdd0dbba2ec8681277ff786e2518c
Yasumasa Ashida <ys.ashida@gmail.com>
2022-02-23 08:35:01 +0900

    chore: [wip] swtich from sprockets to propshaft

--- gems.rb
+++ gems.rb
@@ -4,8 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.0"
 
-# The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
-gem "sprockets-rails"
+# The modern asset pipeline for Rails [https://github.com/rails/propshaft]
+gem "propshaft", "~> 0.6.4"
 
 # Use mysql as the database for Active Record
 gem "mysql2", "~> 0.5.3"
