50bfb12f900e9753da00d1a442040addf075a408
Yasumasa Ashida <ys.ashida@gmail.com>
2022-03-21 09:12:00 +0900

    refactor: app/javascript/components : refactoring of comment_component

--- app/javascript/components/comment_component/component.ts
+++ app/javascript/components/comment_component/component.ts
@@ -18,7 +18,7 @@ var initCommentComponent = (): void => {
 
   const root: HTMLElement = document.querySelector('#comment_component');
 
-  if (root != null) {
+  if (root) {
     Axios.defaults.headers.common = {
       'X-Requested-With': 'XMLHttpRequest',
       'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
