837d1628a7168977f317e524786f006ed4323cf7
Yasumasa Ashida <ys.ashida@gmail.com>
2022-03-26 14:35:46 +0900

    refactor: refactoring of application

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "builds")
-Rails.application.config.assets.paths << Rails.root.join("node_modules")
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+# frozen_string_literal: true
+
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'builds')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
