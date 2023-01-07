efc0c3f4ef4acb48d9d5fea94cca9514ef124d1e
Yasumasa Ashida <ys.ashida@gmail.com>
2023-01-08 00:08:37 +0900

    refactor: app/javascript: refactoring of comment component

--- app/javascript/components/comment_component/component.ts
+++ app/javascript/components/comment_component/component.ts
@@ -12,7 +12,7 @@ let initCommentComponent = (): void => {
   if (root) {
     Axios.defaults.headers.common = {
       'X-Requested-With': 'XMLHttpRequest',
-      'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+      'X-CSRF-TOKEN': constants.csrf_token
     };
 
     const comment_vm: Application = createApp({
--- app/javascript/composables/constants.ts
+++ app/javascript/composables/constants.ts
@@ -1,7 +1,8 @@
 export const root: HTMLElement = document.querySelector('#comment_component');
 
-export const constants: { api_origin: string } = {
-  api_origin: root.getAttribute('data-api-origin')
+export const constants: { api_origin: string, csrf_token: string } = {
+  api_origin: root.getAttribute('data-api-origin'),
+  csrf_token:  document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 }
 
 //console.log(constants.api_origin);
