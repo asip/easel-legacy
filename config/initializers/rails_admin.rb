cf6db996f9fa35c760842f13e868647841668423
Yasumasa Ashida <ys.ashida@gmail.com>
2023-01-30 21:34:59 +0900

    feat: introduce rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -24,6 +24,12 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
+  config.authenticate_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'admin' && password == 'admin'
+    end
+  end
+
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
