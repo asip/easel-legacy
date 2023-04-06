8a6b1f86a2cca40b930f95ea31a0bbee43e2d597
Yasumasa Ashida <ys.ashida@gmail.com>
2023-04-06 14:20:02 +0900

    chore: switch from sassc-rails to dartsass-rails

--- gems.rb
+++ gems.rb
@@ -76,7 +76,7 @@ gem 'rack-cors'
 
 # management console
 gem 'rails_admin', '~> 3.0'
-gem 'sassc-rails'
+gem 'dartsass-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
