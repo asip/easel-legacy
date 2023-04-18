6551c83bb33a86457f74c62a3555b24a8ad3623f
Yasumasa Ashida <ys.ashida@gmail.com>
2023-04-18 17:07:55 +0900

    perf: edit config/initializers/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
-# Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
 # Rails.application.config.assets.paths << Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts')
 
