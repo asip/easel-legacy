0b3dd4c73bd38e32df74c26afc5215035ef38987
Yasumasa Ashida <ys.ashida@gmail.com>
2023-05-15 14:57:09 +0900

    refacor(config): edit settings.yml & settings/*.yml

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [Settings.origin, Settings.api.origin]
+    origins [Settings.origin, Settings.frontend.origin]
 
     resource '*',
              headers: :any,
