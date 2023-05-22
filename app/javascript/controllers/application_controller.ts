272984b0f8cab3494418adc3d0a15d0330df08f6
Yasumasa Ashida <ys.ashida@gmail.com>
2023-05-23 03:50:55 +0900

    fix(js/controller): fix toast_controller

--- app/javascript/controllers/application_controller.ts
+++ app/javascript/controllers/application_controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus'
 
 export default class ApplicationController extends Controller {
-  removeElementsByClassName(className){
+  removeElementsByClassName(className: string){
     const elements: HTMLCollectionOf<Element> = this.element.getElementsByClassName(className)
     Array.from(elements).forEach(e => e.remove())
   }
