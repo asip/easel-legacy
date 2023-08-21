aad5a619c495261f150ef777848ea94881038405
Yasumasa Ashida <ys.ashida@gmail.com>
2023-08-21 11:32:06 +0900

    refactor(controller): edit manager/application_controller
    eliminate override of current_user

--- app/controllers/manager/application_controller.rb
+++ app/controllers/manager/application_controller.rb
@@ -9,10 +9,6 @@ module Manager
     before_action :switch_locale
     before_action :require_login
 
-    def current_user
-      Admin.find_by(id: session[:user_id])
-    end
-
     private
 
     def not_authenticated
