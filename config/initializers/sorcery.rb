626b7d9a5ac6333e785dd734a736df4386c94f33
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-19 12:15:25 +0900

    refactor(config/initializer): refactoring of sorcery.rb

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -450,7 +450,7 @@ Rails.application.config.sorcery.configure do |config|
     # user.provider_uid_attribute_name =
 
     # ...
-    user.jwt_secret = Rails.application.secrets.secret_key_base
+    user.jwt_secret = Rails.application.credentials.secret_key_base
     user.jwt_algorithm = 'HS256' # HS256 is used by default.
     user.session_expiry = 60 * 60 * 2 # 2 weeks is used by default.
   end
