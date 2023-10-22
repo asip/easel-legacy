d45291f85314b12c7dd8647f4ddb20281d9828c7
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-22 23:59:13 +0900

    fix(model): fix models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,13 +62,12 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A\z|\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   with_options on: :with_validation do |save|
-    save.validates :name, presence: true, length: { in: 3..40 } # , format: { with: VALID_NAME_REGEX }
-
-    save.validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-
     save.validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
     save.validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
     save.validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+    save.validates :name, presence: true, length: { in: 3..40 } # , format: { with: VALID_NAME_REGEX }
+    save.validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   end
 
   with_options on: :login do
