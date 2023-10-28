6c96ff4e29edb6a25057c25fb44721b3700a9327
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-28 18:18:45 +0900

    comment(model): edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -102,17 +102,17 @@ class User < ApplicationRecord
     update!(token: nil)
   end
 
-  # フォローしたときの処理
+  # (フォローしたときの処理)
   def follow(user_id)
     follower_relationships.create(followee_id: user_id)
   end
 
-  # フォローを外すときの処理
+  # (フォローを外すときの処理)
   def unfollow(user_id)
     follower_relationships.find_by(followee_id: user_id).destroy
   end
 
-  # フォローしているか判定
+  # (フォローしているか判定)
   def following?(user)
     followees.include?(user)
   end
