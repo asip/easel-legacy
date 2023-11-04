226de289adf67df234fbe553e7a0bad406eaf850
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-04 12:50:36 +0900

    refactor(model): edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -97,11 +97,9 @@ class User < ApplicationRecord
     full_error_messages_for(%i[image name email password password_confirmation])
   end
 
-  # rubocop:disable Rails/SkipsModelValidations
   def assign_token(token_)
-    update_attribute(:token, token_)
+    update_attribute!(:token, token_)
   end
-  # rubocop:enable Rails/SkipsModelValidations
 
   def update_token
     return unless saved_change_to_email?
