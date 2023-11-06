e298c9be8f034e25293d5f94301f9394717d1ec0
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-06 23:50:37 +0900

    refactor(controller/concern): rename pagination
    pagination -> api/pagination

---
+++ app/controllers/concerns/api/pagination.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# api
+module Api
+  # Pagination module
+  module Pagination
+    extend ActiveSupport::Concern
+
+    def resources_with_pagination(resources)
+      {
+        meta: {
+          pagination: {
+            count: resources.total_count,
+            pages: resources.total_pages,
+            page: resources.current_page,
+            per: resources.limit_value
+          }
+        }
+      }
+    end
+  end
+end
