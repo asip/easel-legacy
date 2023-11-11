c2437dd18a386f7fe18ab21428f784e4f502f257
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-11 21:05:24 +0900

    chore: update rails to 7.1.2

--- gems.rb
+++ gems.rb
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem 'rails', '~> 7.1.1'
+gem 'rails', '~> 7.1.2'
 
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem 'propshaft', '~> 0.7.0'
