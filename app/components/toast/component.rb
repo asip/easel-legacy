ea1343696198e132f54eb7d739e772da6a6c303a
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-13 17:01:38 +0900

    feat(component): add flash support to toast component

--- app/components/toast/component.rb
+++ app/components/toast/component.rb
@@ -4,10 +4,22 @@
 module Toast
   # Component
   class Component < ViewComponent::Base
-    def initialize(flashes:)
+    def initialize(flashes: {}, flash: {})
       super
       @flashes = flashes
+      @flash = flash
       @flashes_json = flashes.to_json
+      @flash_json = flash_to_json
+    end
+
+    private
+
+    def flash_to_json
+      flash_ = {}
+      @flash.each  do |key, value|
+        flash_[key] = [value]
+      end
+      flash_.to_json
     end
   end
 end
