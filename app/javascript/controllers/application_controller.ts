bf6463449b461c666a8226a9f9d7f45d872c1464
Yasumasa Ashida <ys.ashida@gmail.com>
2023-12-16 18:23:51 +0900

    refactor(js/controller): edit application_controller

--- app/javascript/controllers/application_controller.ts
+++ app/javascript/controllers/application_controller.ts
@@ -2,7 +2,7 @@ import { Controller } from 'stimulus'
 
 export default class ApplicationController extends Controller {
   removeElementsByClassName(className: string){
-    const elements: HTMLCollectionOf<Element> = this.element.getElementsByClassName(className)
+    const elements: NodeListOf<Element> = this.element.querySelectorAll(`.${className}`)
     Array.from(elements).forEach(e => { e.remove() })
   }
 }
