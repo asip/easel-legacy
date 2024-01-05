6c826718ae4d52a7e60d0b756e8eae6bb58b1db4
Yasumasa Ashida <ys.ashida@gmail.com>
2024-01-05 10:04:36 +0900

    chore: introduce rubocop-rails-omakase gem

--- gems.rb
+++ gems.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem 'rails', '~> 7.1.2'
+gem "rails", "~> 7.1.2"
 
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
-gem 'propshaft', '~> 0.8.0'
+gem "propshaft", "~> 0.8.0"
 
 # gem 'dartsass-rails'
 
 # Use mysql as the database for Active Record
 # gem 'mysql2', '~> 0.5.3'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 1.5.3'
+gem "pg", "~> 1.5.3"
 
 # Use the Puma web server [https://github.com/puma/puma]
-gem 'puma', '~> 6.4.0'
+gem "puma", "~> 6.4.0"
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
-gem 'turbo-rails'
+gem "turbo-rails"
 
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
-gem 'stimulus-rails'
+gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
@@ -31,92 +31,90 @@ gem 'stimulus-rails'
 # gem "image_processing", "~> 1.2"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: %i[windows jruby]
+gem "tzinfo-data", platforms: %i[windows jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', require: false
+gem "bootsnap", require: false
 
-gem 'cssbundling-rails'
-gem 'jsbundling-rails'
+gem "cssbundling-rails"
+gem "jsbundling-rails"
 
 # view component
-gem 'view_component'
+gem "view_component"
 
 # Shrine
-gem 'aws-sdk-s3', '~> 1.14'
-gem 'image_processing', '~> 1.8'
-gem 'shrine', '~> 3.0'
+gem "aws-sdk-s3", "~> 1.14"
+gem "image_processing", "~> 1.8"
+gem "shrine", "~> 3.0"
 
 # i18n
-gem 'rails-i18n', '~> 7.0.0'
+gem "rails-i18n", "~> 7.0.0"
 
 # error page handling
-gem 'rambulance'
+gem "rambulance"
 
 # authentication
-gem 'sorcery'
-gem 'sorcery-jwt'
+gem "sorcery"
+gem "sorcery-jwt"
 
-gem 'googleauth'
+gem "googleauth"
 
 # settings
-gem 'config'
+gem "config"
 
 # paging
-gem 'pagy', '~> 6.1.0'
+gem "pagy", "~> 6.1.0"
 # gem 'bootstrap5-kaminari-views'
 # gem 'kaminari', '~> 1.2.2'
 
 # tags
 # gem "acts-as-taggable-on", github: "mbleigh/acts-as-taggable-on"
-gem 'acts-as-taggable-on', '~> 10.0.0'
+gem "acts-as-taggable-on", "~> 10.0.0"
 
 # json
-gem 'jsonapi-serializer'
+gem "jsonapi-serializer"
 
 # Rack::Locale
-gem 'rack-contrib', '2.4.0'
+gem "rack-contrib", "2.4.0"
 
 # cors
-gem 'rack-cors'
+gem "rack-cors"
 
 # soft delete
-gem 'discard', '~> 1.2'
+gem "discard", "~> 1.2"
 
 # management console
-gem 'rails_admin', '~> 3.0'
+gem "rails_admin", "~> 3.0"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'debug', platforms: %i[mri windows]
+  gem "debug", platforms: %i[mri windows]
   # Ruby style guide, linter, and formatter
-  gem 'rspec-rails', '~> 6.1'
+  gem "rspec-rails", "~> 6.1"
 end
 
 group :development do
-  gem 'brakeman'
-  gem 'bullet'
-  gem 'pg_query'
-  gem 'prosopite'
+  gem "brakeman"
+  gem "bullet"
+  gem "pg_query"
+  gem "prosopite"
 
   # Use console on exceptions pages [https://github.com/rails/web-console]
-  gem 'web-console'
+  gem "web-console"
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
-  gem 'annotate'
-  gem 'rails-erd'
+  gem "annotate"
+  gem "rails-erd"
 
-  gem 'rubocop-performance', require: false
-  gem 'rubocop-rails', require: false
-  # gem 'rubocop-sensei', require: false
+  gem "rubocop-rails-omakase", require: false
   # Shopify/erb-lint
-  gem 'erb_lint', require: false
+  gem "erb_lint", require: false
 end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem 'capybara'
-  gem 'selenium-webdriver'
+  gem "capybara"
+  gem "selenium-webdriver"
 end
