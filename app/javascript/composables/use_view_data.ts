1386dfe0d290c4c452edc9a782ac5f4283ca9ddc
Yasumasa Ashida <ys.ashida@gmail.com>
2024-02-21 11:30:54 +0900

    refactor(js/composable): edit use_view_data

--- app/javascript/composables/use_view_data.ts
+++ app/javascript/composables/use_view_data.ts
@@ -1,24 +1,14 @@
 declare var document: Document
 
-interface ViewData {
-  root: HTMLElement | null
-  api_origin: string | null | undefined
-  csrf_token: string | null | undefined
-  frame_id: number | null
-}
-
 export function useViewData(){
   const root: HTMLElement | null = document.querySelector('#comments_component')
 
   const str_frame_id = root?.getAttribute('data-frame-id')
 
-  const viewData: ViewData = {
-    root: root,
-    api_origin: root?.getAttribute('data-api-origin'),
-    csrf_token: document.querySelector('meta[name="csrf-token"]')?.getAttribute('content'),
-    frame_id: str_frame_id ? parseInt(str_frame_id, 10) : null
-  }
-  //console.log(viewData.api_origin);
+  const api_origin: string | null | undefined = root?.getAttribute('data-api-origin')
+  const csrf_token: string | null | undefined = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content')
+  const frame_id: number | null = str_frame_id ? parseInt(str_frame_id, 10) : null
 
-  return viewData
+  return { root, api_origin, csrf_token, frame_id }
 }
+
