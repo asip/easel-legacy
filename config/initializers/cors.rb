a5f2dcb6fa0bda4af1aceea2a00075350eb3023b
Yasumasa Ashida <ys.ashida@gmail.com>
2024-04-18 23:41:12 +0900

    chore(config): edit initializers/cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [ Settings.origin ]
+    origins [ Settings.origin, "#{Settings.origin}:3000" ]
 
     resource "*",
              headers: :any,
