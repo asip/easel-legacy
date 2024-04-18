b9f8b46682c7f1784bcc5cfc2c977bc7b58d1599
Yasumasa Ashida <ys.ashida@gmail.com>
2024-04-19 01:55:35 +0900

    style(config): edit webpack/webpack.config.js

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,11 +1,12 @@
-import { fileURLToPath } from "node:url";
-import path from "node:path"
-import webpack from "webpack"
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import webpack from 'webpack'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+// eslint-disable-next-line no-undef
+const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production'
 
 export default {
   mode,
@@ -16,12 +17,12 @@ export default {
     extensions: ['.js', '.ts']
   },
   entry: {
-    application: "./app/javascript/application.js",
-    web_components: "./app/javascript/web_components.js"
+    application: './app/javascript/application.js',
+    web_components: './app/javascript/web_components.js'
   },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[name].js.map",
+    filename: '[name].js',
+    sourceMapFilename: '[name].js.map',
     path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
   },
   plugins: [
