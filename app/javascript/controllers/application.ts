2cf5633e6cc4a3e2bb629fe2a5cc0c5ae4c2e958
Yasumasa Ashida <ys.ashida@gmail.com>
2025-04-21 21:41:51 +0900

    chore: switch from luminous-lightbox to baguettebox.js

--- app/javascript/controllers/application.ts
+++ app/javascript/controllers/application.ts
@@ -5,7 +5,7 @@ import { Application } from '@hotwired/stimulus'
 //application.load(definitionsFromContext(context))
 import CalendarController from './calendar_controller'
 import TagifyController from './tagify_controller'
-import LuminousController from './luminous_controller'
+import LightboxController from './lightbox_controller'
 import PhotoSwipeController from './photo_swipe_controller'
 import PreviewController from './image/preview_controller'
 import ToastController from './toast_controller'
@@ -17,7 +17,7 @@ application.debug = false
 
 application.register('calendar', CalendarController)
 application.register('tagify', TagifyController)
-application.register('luminous', LuminousController)
+application.register('lightbox', LightboxController)
 application.register('pswipe', PhotoSwipeController)
 application.register('preview', PreviewController)
 application.register('toast', ToastController)
---
+++ app/javascript/controllers/lightbox_controller.ts
@@ -0,0 +1,35 @@
+import ApplicationController from './application_controller'
+// @ts-expect-error : @types doesn't exist
+import baguetteBox from 'baguettebox.js'
+
+export default class LightboxController extends ApplicationController {
+  // static targets = ['lb']
+
+  // declare readonly lbTarget: HTMLElement
+
+  lightbox: any
+
+  // declare readonly hasLbTarget: boolean
+
+  connect() {
+    /*
+    let lb_trigger: HTMLElement | null = null
+    if(this.hasLbTarget){
+      lb_trigger = this.lbTarget
+    }
+    */
+
+    //if(lb_trigger){
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    this.lightbox = baguetteBox.run('.lb')
+    //}
+  }
+
+  disconnect(){
+    // eslint-disable-next-line no-undef
+    if(this.lightbox && document.querySelector('#baguetteBox-overlay')){
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      baguetteBox.destroy() 
+    }
+  }
+}
