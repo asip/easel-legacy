3beab0d8f42fb1c1a3328d7eda532d57bd9dbae4
Yasumasa Ashida <ys.ashida@gmail.com>
2025-05-21 20:47:14 +0900

    refactor(controller): Refactor admins/sessions_controller

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -17,8 +17,10 @@ class Admins::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate(auth_options)
-    set_flash_message!(:notice, :signed_in) if resource
-    sign_in(resource_name, resource) if resource
+    if resource
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+    end
     # yield resource if block_given?
     respond_with resource
   end
