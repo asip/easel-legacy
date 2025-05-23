51e328098c63df8fb4e841fb73d29e1cf2cdd790
Yasumasa Ashida <ys.ashida@gmail.com>
2025-05-24 03:22:39 +0900

    remove(config/initializer): remove requirements.rb

--- config/initializers/requirements.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require Rails.root.join("app/helpers/application_helper.rb")
-require Rails.root.join("app/helpers/frame_helper.rb")
-require Rails.root.join("app/helpers/sessions_helper.rb")
-require Rails.root.join("app/helpers/users_helper.rb")
