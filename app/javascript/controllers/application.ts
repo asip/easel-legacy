bd2bf821635083ff80092732cee5e841746c049d
Yasumasa Ashida <ys.ashida@gmail.com>
2025-06-24 19:01:42 +0900

    update(js/controller): update application.ts

--- app/javascript/controllers/application.ts
+++ app/javascript/controllers/application.ts
@@ -3,23 +3,27 @@ import { Application } from '@hotwired/stimulus'
 //import { definitionsFromContext } from "stimulus/webpack-helpers"
 //const context = require.context(".", true, /_controller\.js$/)
 //application.load(definitionsFromContext(context))
+import ToastController from './toast-controller'
 import CalendarController from './calendar-controller'
-import TagifyController from './tagify-controller'
+// image preview
+import PreviewController from './image/preview-controller'
+// frame
 import LightboxController from './lightbox-controller'
 import PhotoSwipeController from './photo-swipe-controller'
-import PreviewController from './image/preview-controller'
-import ToastController from './toast-controller'
+import TagifyController from './tagify-controller'
 
 const application: Application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 
+application.register('toast', ToastController)
 application.register('calendar', CalendarController)
-application.register('tagify', TagifyController)
+// image preview
+application.register('preview', PreviewController)
+// frame
 application.register('lightbox', LightboxController)
 application.register('pswipe', PhotoSwipeController)
-application.register('preview', PreviewController)
-application.register('toast', ToastController)
+application.register('tagify', TagifyController)
 
 export { application }
