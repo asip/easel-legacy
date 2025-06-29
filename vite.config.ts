82c5b35fb0747db44d815ae55aefce4dba8bbfcb
Yasumasa Ashida <ys.ashida@gmail.com>
2025-06-29 19:14:43 +0900

    chore: Introduce vite_rails

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     vue(),
     tailwindcss()
-  ]
+  ],
+  resolve: {
+    alias: {
+      'vanillajs-datepicker': path.resolve(__dirname, 'node_modules/vanillajs-datepicker'),
+    },
+  },
 })
