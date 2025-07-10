401f5128657539f52f8742ddf39186711ab84b97
Yasumasa Ashida <ys.ashida@gmail.com>
2025-07-10 09:45:35 +0900

    refactor(controller/concern): Refactor api/error_renderable
    refactor(controller/concern): Refactor api/error_renderable

--- app/controllers/concerns/api/error_renderable.rb
+++ app/controllers/concerns/api/error_renderable.rb
@@ -7,48 +7,66 @@ module Api
     extend ActiveSupport::Concern
 
     included do
+      # Standard Error
       rescue_from StandardError, with: ->(e) { request.format.json? ? render500(e) : raise(e) }
+
+      # ActiveRecord Error
       rescue_from ActiveRecord::RecordNotFound, with: ->(e) { request.format.json? ? render404(e) : raise(e) }
       rescue_from ActiveRecord::RecordNotUnique, with: ->(e) { request.format.json? ? render409(e) : raise(e) }
       rescue_from ActiveRecord::RecordInvalid, with: ->(e) { request.format.json? ? render422(e) : raise(e) }
+
+      # UnauthorizedError
       rescue_from UnauthorizedError, with: ->(e) { request.format.json? ? render401(e) : raise(e) }
     end
 
+    # Custom error class for unauthorized access (未認証アクセス用のカスタムエラークラス)
     class UnauthorizedError < StandardError
+      def initialize(message = "Authentication information is invalid.")
+        super(message)
+      end
     end
 
     protected
 
-    def render400(exception = nil, messages = nil)
+    # Common helper methods for rendering error response (エラーレスポンスをレンダリングする共通のヘルパーメソッド
+
+    # HTTP Status 400 Bad Request
+    def render400(exception = nil, *messages)
       render_error(400, "Bad Request", exception&.message, *messages)
     end
 
-    def render401(exception = nil, messages = nil)
+    # HTTP Status 401 Unauthorized
+    def render401(exception = nil, *messages)
       render_error(401, "Unauthorized", exception&.message, *messages)
     end
 
-    def render404(exception = nil, messages = nil)
+    # HTTP Status 404 Not Found
+    def render404(exception = nil, *messages)
       render_error(404, "Not Found", exception&.message, *messages)
     end
 
-    def render409(exception = nil, messages = nil)
+    # HTTP Status 409 Conflict
+    def render409(exception = nil, *messages)
       render_error(409, "Conflict", exception&.message, *messages)
     end
 
-    def render422(exception = nil, messages = nil)
+    # HTTP Status 422 Unprocessable Entity
+    def render422(exception = nil, *messages)
       render_error(422, "Unprocessable Entity", exception&.message, *messages)
     end
 
-    def render500(exception = nil, messages = nil)
+    # HTTP Status 500 Internal Server Error
+    def render500(exception = nil, *messages)
       logger.error exception.full_message
-      # logger.error exception.backtrace
+      # logger.error exception.backtrace.join("\n") # backtrace
+
       render_error(500, "Internal Server Error", exception&.message, *messages)
     end
 
-    def render_error(code, message, *error_messages)
+    def render_error(code, default_message, *error_messages)
       response = {
-        message:,
-        errors: error_messages.compact
+        message: default_message,
+        errors: error_messages.compact.uniq
       }
 
       render json: response, status: code
