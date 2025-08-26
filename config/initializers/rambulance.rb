8fdf8b57816b4516e4b4e50a5d261dcc3de8b7ad
Yasumasa Ashida <ys.ashida@gmail.com>
2025-08-26 20:20:08 +0900

    degin(view): Update page design of error pages

--- config/initializers/rambulance.rb
+++ config/initializers/rambulance.rb
@@ -12,8 +12,8 @@ Rambulance.setup do |config|
   #   ActionController::UnknownFormat                    => :not_acceptable,
   #   ActionDispatch::Http::MimeNegotiation::InvalidType => :not_acceptable,
   #   ActionController::MissingExactTemplate             => :not_acceptable,
-  #   ActionController::InvalidAuthenticityToken         => :unprocessable_entity,
-  #   ActionController::InvalidCrossOriginRequest        => :unprocessable_entity,
+  #   ActionController::InvalidAuthenticityToken         => :unprocessable_content,
+  #   ActionController::InvalidCrossOriginRequest        => :unprocessable_content,
   #   ActionDispatch::Http::Parameters::ParseError       => :bad_request,
   #   ActionController::BadRequest                       => :bad_request,
   #   ActionController::ParameterMissing                 => :bad_request,
@@ -21,8 +21,8 @@ Rambulance.setup do |config|
   #   Rack::QueryParser::InvalidParameterError           => :bad_request,
   #   ActiveRecord::RecordNotFound                       => :not_found,
   #   ActiveRecord::StaleObjectError                     => :conflict,
-  #   ActiveRecord::RecordInvalid                        => :unprocessable_entity,
-  #   ActiveRecord::RecordNotSaved                       => :unprocessable_entity
+  #   ActiveRecord::RecordInvalid                        => :unprocessable_content,
+  #   ActiveRecord::RecordNotSaved                       => :unprocessable_content
   #
   # If you add exceptions in this config, Rambulance uses the pairs you defined
   # here *in addition* to the default mappings. You can also override the default
@@ -30,7 +30,7 @@ Rambulance.setup do |config|
   # If Rambulance receives an exception that is not listed here, it'll render
   # the internal server error template and return 500 as http status.
   config.rescue_responses = {
-    # "ActiveRecord::RecordNotUnique" => :unprocessable_entity,
+    # "ActiveRecord::RecordNotUnique" => :unprocessable_content,
     # "CanCan::AccessDenied"          => :forbidden,
     # "Pundit::NotAuthorizedError"    => :forbidden,
     # "YourCustomException"           => :not_found
