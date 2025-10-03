705daac0409ab6b278c27e7b6a6aadc46c23d604
Yasumasa Ashida <ys.ashida@gmail.com>
2025-10-03 14:06:08 +0900

    refactor(interaction): Refactor mutations/comments/*

--- app/interactions/mutations/comments/create_comment.rb
+++ app/interactions/mutations/comments/create_comment.rb
@@ -18,7 +18,8 @@ module Mutations
 
       def execute
         comment = Comment.new(@form_params)
-        mutation = Mutations::Comments::SaveComment.run(user: @user, frame_id: @frame_id, comment:)
+        comment.frame_id = @frame_id
+        mutation = Mutations::Comments::SaveComment.run(user: @user, comment:)
         errors.merge!(mutation.errors) unless mutation.success?
         @comment = mutation.comment
       end
--- app/interactions/mutations/comments/save_comment.rb
+++ app/interactions/mutations/comments/save_comment.rb
@@ -10,14 +10,12 @@ module Mutations
 
       attr_reader :comment
 
-      def initialize(user:, frame_id:, comment:)
+      def initialize(user:, comment:)
         @user = user
-        @frame_id = frame_id
         @comment = comment
       end
 
       def execute
-        @comment.frame_id = @frame_id
         @comment.user_id = @user.id
         return if @comment.save
 
