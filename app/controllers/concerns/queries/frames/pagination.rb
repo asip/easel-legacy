5bdec44e89bf66dd10c4272ec5bee0ced00912bc
Yasumasa Ashida <ys.ashida@gmail.com>
2025-10-09 21:26:31 +0900

    refactor(controller/concerns): Refactor queries/frames/pagination

--- app/controllers/concerns/queries/frames/pagination.rb
+++ app/controllers/concerns/queries/frames/pagination.rb
@@ -13,8 +13,7 @@ module Queries
       protected
 
       def list_frames(items:, page:)
-        frames = Queries::Frames::ListFrameIds.run(items:)
-        frame_ids = frames.pluck(:id)
+        frame_ids = Queries::Frames::ListFrameIds.run(items:)
         frames = Frame.where(id: frame_ids).order(created_at: "desc")
         pagy, frames = pagy(frames, page:)
         [ pagy, frames ]
