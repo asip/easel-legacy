610813fdd786552ab4c3c27c767b2f95dffd2ed9
Yasumasa Ashida <ys.ashida@gmail.com>
2025-10-20 14:56:52 +0900

    refactor(frontend/controller): Refactor image/preview-controller

--- app/frontend/controllers/image/preview-controller.ts
+++ app/frontend/controllers/image/preview-controller.ts
@@ -27,27 +27,32 @@ export default class PreviewController extends ApplicationController {
 
     if (elmUpload) {
       elmUpload.addEventListener('change', function () {
-        const file: { name?: string, ext?: string, data?: Blob } = {}
-        file.name = this.value
-        file.ext = file.name.replace(/^.*\./, '').toLowerCase()
-        // .file_fieldからデータを取得して変数file.dataに代入します
-        file.data = this.files![0]
+        // let blob: Blob | null| undefined
+        const file: { data?: File | null } = {}
 
-        if (file.ext.match(/^(jpeg|jpg|png|gif)$/)) {
-          // FileReaderオブジェクトを作成します
+        // (.file_fieldからデータを取得して変数file.dataに代入します)
+        file.data = this.files?.item(0)
+        /*
+        if (file.data) {
+          const buffer = await file.data.arrayBuffer()
+          blob = new Blob([buffer], { type: file.data.type })
+        }
+        */
+        if (file.data?.type.match(/^image\/(jpeg|jpg|png|gif)$/)) {
+          // (FileReaderオブジェクトを作成します)
           const reader = new FileReader()
-          // 読み込みが完了したら処理が実行されます
+          // (読み込みが完了したら処理が実行されます)
           reader.onload = function () {
-            // 読み込んだファイルの内容を取得して変数imageに代入します
+            // (読み込んだファイルの内容を取得して変数imageに代入します)
             const image: string | ArrayBuffer | null = this.result
             //console.log(content.style.display);
             if (preview) { preview.src = image as string }
-            // プレビュー画像がなければ表示します
+            // (プレビュー画像がなければ表示します)
             if (content && content.style.display == 'none') {
               content.style.display = 'block'
             }
           }
-          // DataURIScheme文字列を取得します
+          // (DataURIScheme文字列を取得します)
           reader.readAsDataURL(file.data)
         } else {
           if (content) { content.style.display = 'none' }
