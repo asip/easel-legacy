318e752b72df21d9024da9238fef37f50d57043e
Yasumasa Ashida <ys.ashida@gmail.com>
2025-11-09 07:35:18 +0900

    refactor(component): Refactor preview/tags

---
+++ app/components/preview/tags/component.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# preview
+module Preview
+  # tags
+  module Tags
+    # Component
+    class Component < ViewComponent::Base
+      def initialize(tag_paths:, list: false, link: true)
+        @tag_paths = tag_paths
+        @list = list
+        @link = link
+      end
+    end
+  end
+end
