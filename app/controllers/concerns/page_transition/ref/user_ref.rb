35e707b9c729c94db330743f815a7af71032664d
Yasumasa Ashida <ys.ashida@gmail.com>
2025-11-28 21:32:56 +0900

    refactor: Refactor around search criteria

--- app/controllers/concerns/page_transition/ref/user_ref.rb
+++ app/controllers/concerns/page_transition/ref/user_ref.rb
@@ -20,19 +20,18 @@ module PageTransition
           items = ref_items
           case items[:from]
           when "frame"
-            frame_path(Frame.find(items[:id]), Ref::UserRef.query_from(ref_items: items, q_items: permitted_params[:q]))
+            frame_path(Frame.find(items[:id]), Ref::UserRef.query_from(ref_items: items))
           else
             prev_url
           end
         }.call
       end
 
-      def self.query_from(ref_items:, q_items:)
+      def self.query_from(ref_items:)
         page = ref_items[:page]
         items = {}
         items[:page] = page if page.present?
         query = {}
-        query[:q] = q_items if q_items.present?
         query[:ref] = items.to_json if items.present?
         query
       end
