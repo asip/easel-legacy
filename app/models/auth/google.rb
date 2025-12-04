72d4c54fc93c38af8ad79b45ddab3546a2de80d9
Yasumasa Ashida <ys.ashida@gmail.com>
2025-12-04 12:13:46 +0900

    fix(model): Fix typo in auth/google

--- app/models/auth/google.rb
+++ app/models/auth/google.rb
@@ -7,7 +7,7 @@ module Auth
     def self.auth_from(credential:, provider:)
       auth = {}
       auth[:info] = ::Google::Auth::IDTokens.verify_oidc(credential,
-                                                       aud: Settings.google.client_id)
+                                                         aud: Settings.google.client_id)
                                             .with_indifferent_access
       auth[:uid] = auth[:info][:sub]
       auth[:provider] = provider
