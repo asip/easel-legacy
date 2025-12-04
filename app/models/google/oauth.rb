621cd0e69540ed645fdc49df86a73811dbbc70bc
Yasumasa Ashida <ys.ashida@gmail.com>
2025-12-04 11:47:09 +0900

    refactor(model/concern): Refactor login/user

--- app/models/google/oauth.rb
+++ app/models/google/oauth.rb
@@ -3,7 +3,7 @@ module Google
     def self.auth_from(credential:, provider:)
       auth = {}
       auth[:info] = Google::Auth::IDTokens.verify_oidc(credential,
-                                                         aud: Settings.google.client_id)
+                                                       aud: Settings.google.client_id)
                                             .with_indifferent_access
       auth[:uid] = auth[:info][:sub]
       auth[:provider] = provider
