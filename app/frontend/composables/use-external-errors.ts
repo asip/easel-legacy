4180573a681dc11e280d2069955e7ad980a4c8d4
Yasumasa Ashida <ys.ashida@gmail.com>
2025-12-05 15:21:30 +0900

    refactor(frontend/composable): Refactor model/use-comment

--- app/frontend/composables/use-external-errors.ts
+++ app/frontend/composables/use-external-errors.ts
@@ -1,7 +1,7 @@
 import { ref, type Ref } from 'vue'
 
 import { ErrorMessages } from '../types'
-import { Flash } from '../interfaces'
+import type { Flash } from '../interfaces'
 
 export function useExternalErrors<ErrorProperty extends string>({ flash } : { flash: Ref<Flash> }) {
   const externalErrors = ref<ErrorMessages<ErrorProperty>>({})
