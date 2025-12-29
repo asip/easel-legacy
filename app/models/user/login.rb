7ccea012f8e4e673bd48c9a41f1d79d5cb188303
Yasumasa Ashida <ys.ashida@gmail.com>
2025-12-29 10:36:05 +0900

    refactor(model/concern): Refactor concerns

--- app/models/user/login.rb
+++ app/models/user/login.rb
@@ -21,15 +21,6 @@ module User::Login
     errors.add(:email, I18n.t("action.login.invalid")) if form[:email].present? && !errors.include?(:email)
   end
 
-  def enable_with(auth:)
-    email = auth.email
-
-    self.email = email if email.present? && self.email != email
-    self.deleted_at = nil
-    self.save!
-    self
-  end
-
   class_methods do
     def validate_on_login(form:)
       user = ::User.find_for_authentication(email: form[:email])
@@ -41,39 +32,6 @@ module User::Login
       end
       success = user.errors.empty?
       [ success, user ]
-    end
-
-    def from(auth:, time_zone:)
-      uid = auth.uid
-      provider = auth.provider
-
-      # (認証レコードを検索)
-      authentication = ::Authentication.find_by(uid:, provider:)
-
-      if authentication
-        user = ::User.unscoped.find_by(id: authentication.user_id)
-        user&.enable_with(auth:)
-      else
-        user = find_or_create_from(auth:, time_zone:)
-        ::Authentication.create_from(user:, provider:, uid:)
-      end
-
-      user
-    end
-
-    def find_or_create_from(auth:, time_zone:)
-      email = auth.email
-      name = auth.name
-
-      user = ::User.unscoped.find_for_authentication(email:)
-
-      unless user
-        user = ::User.new(name:, email:, password: Devise.friendly_token[0, 20], time_zone:)
-      else
-        user.deleted_at = nil
-      end
-      user.save!
-      user
     end
   end
 end
