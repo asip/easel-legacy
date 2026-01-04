d40fdb910293322e6868292e785e8090c5528266
Yasumasa Ashida <ys.ashida@gmail.com>
2026-01-04 18:06:35 +0900

    refactor(view): Refactor layouts/error

---
+++ app/components/errors/style/component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# errors
+module Errors
+  # style
+  module Style
+    # Component
+    class Component < ViewComponent::Base
+      def initialize
+      end
+    end
+  end
+end
+
