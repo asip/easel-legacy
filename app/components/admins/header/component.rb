df84c76e2bc9daec334ec8e869efd32dda5ae4a9
Yasumasa Ashida <ys.ashida@gmail.com>
2026-01-04 16:23:03 +0900

    refactor(view): Refactor layouts/*

---
+++ app/components/admins/header/component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# admins
+module Admins
+  # header
+  module Header
+    # Component
+    class Component < ViewComponent::Base
+      def initialize
+      end
+    end
+  end
+end
---
+++ app/components/header/component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# header
+module Header
+  # Component
+  class Component < ViewComponent::Base
+    def initialize
+    end
+  end
+end
