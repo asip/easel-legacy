94e272204f6199464f900946aa2b8788bf83cf97
Yasumasa Ashida <ys.ashida@gmail.com>
2026-02-12 16:27:39 +0900

    refactor(component): Refactor toast component

--- app/components/toast/component.rb
+++ app/components/toast/component.rb
@@ -5,25 +5,17 @@ module Toast
   # Component class
   class Component < ViewComponent::Base
     def initialize(flash: {}, flashes: {})
-      @flashes_json = flashes_to_json(flash:, flashes:)
+      @json = to_json(flash:, flashes:)
     end
 
     private
 
-    def flashes_to_json(flash:, flashes:)
+    def to_json(flash:, flashes:)
       if flash.present?
-        to_json(flash:)
+        Oj.dump(flash.to_h.transform_values { |value| [ value ] })
       elsif flashes.present?
         Oj.dump(flashes)
       end
-    end
-
-    def to_json(flash:)
-      flash_ = {}
-      flash.each do |key, value|
-        flash_[key] = [ value ]
-      end
-      Oj.dump(flash_)
     end
   end
 end
