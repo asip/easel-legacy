499e2388fff639352c4053530a746ddd536b6084
Yasumasa Ashida <ys.ashida@gmail.com>
2026-02-19 18:25:49 +0900

    refactor(frontend/composable): Refactor use-locale

--- app/frontend/composables/foundation/use-locale.ts
+++ app/frontend/composables/foundation/use-locale.ts
@@ -1,7 +1,7 @@
 import { i18n } from '~/i18n'
 
 export const useLocale = () => {
-  const { locale, availableLocales } = i18n.global
+  const { locale, availableLocales, fallbackLocale } = i18n.global
 
   const autoDetect = (): void => {
     const viewLocale = globalThis.navigator.languages[0]
@@ -9,7 +9,7 @@ export const useLocale = () => {
     type AvailableLocales = (typeof availableLocales)[number]
 
     locale.value = (
-      (availableLocales as string[]).includes(viewLocale) ? viewLocale : 'en'
+      (availableLocales as string[]).includes(viewLocale) ? viewLocale : fallbackLocale.value
     ) as AvailableLocales
   }
 
