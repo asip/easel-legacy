ab5e6991db99ed8b773bac6e48857242314395bf
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-01 12:29:28 +0900

    refactor(frontend/composable): Refactor logic/use-frame-search

--- app/frontend/composables/logic/use-frame-search.ts
+++ app/frontend/composables/logic/use-frame-search.ts
@@ -1,27 +1,17 @@
 import * as v from 'valibot'
-import { ref } from '@vue/reactivity'
+import { ref, computed } from '@vue/reactivity'
 
 import { i18n } from '~/i18n'
 
 import { useLocale, useCookie } from '~/composables'
 import { useSearchCriteria } from '~/stores'
 
-interface FrameSearchOptions {
-  el?: Element
-}
-
-interface SearchOptions {
-  ev: Event
-  message: string
-}
-
 interface SearchPropertys {
   word?: string | null
   tagName?: string | null
-  q?: string
 }
 
-export function useFrameSearch(options?: FrameSearchOptions) {
+export function useFrameSearch(options?: { el?: Element }) {
   const { autoDetect } = useLocale()
   const { criteria } = useSearchCriteria()
 
@@ -34,9 +24,22 @@ export function useFrameSearch(options?: FrameSearchOptions) {
 
   const searchParams = ref<SearchPropertys>({})
 
-  const errors = ref<SearchPropertys>({ word: '', tagName: '' })
+  const queryMap = computed<{ q?: string }>(() => {
+    const items = searchParams.value
+    const query: { q?: string } = {}
 
-  const success = ref<boolean>()
+    if (items.word) {
+      query.q = JSON.stringify({ word: items.word })
+    } else if (items.tagName) {
+      query.q = JSON.stringify({ tag_name: items.tagName })
+    } else {
+      query.q = JSON.stringify({})
+    }
+
+    return query
+  })
+
+  const errors = ref<SearchPropertys>({ word: '', tagName: '' })
 
   const initSearchParams = () => {
     searchParams.value.word = criteria.value.word
@@ -44,53 +47,32 @@ export function useFrameSearch(options?: FrameSearchOptions) {
   }
 
   const search = (ev: Event): void => {
-    const errorMessages = validateSearchParams()
+    const success = validateSearchParams()
 
-    if (searchParams.value.word) {
-      searchByWord({ ev, message: errorMessages?.word?.at(0) ?? '' })
+    if (success) {
+      submit()
     } else {
-      searchByTagName({ ev, message: errorMessages?.tagName?.at(0) ?? '' })
+      ev.preventDefault()
     }
   }
 
   const validateSearchParams = () => {
     const result = v.safeParse(schema, searchParams.value, { lang: i18n.global.locale.value })
     const errorMessages = result.issues ? v.flatten(result.issues).nested : {}
-    success.value = result.success
+    const success = result.success
 
-    return errorMessages
-  }
+    errors.value.word = errorMessages?.word?.at(0) ?? ''
+    errors.value.tagName = errorMessages?.tagName?.at(0) ?? ''
 
-  const searchByWord = ({ ev, message }: SearchOptions): void => {
-    if (success.value) {
-      searchParams.value.q = searchParams.value.word
-        ? JSON.stringify({ word: searchParams.value.word ?? '' })
-        : '{}'
-      submit()
-    } else {
-      errors.value.word = message
-      ev.preventDefault()
-    }
-  }
-
-  const searchByTagName = ({ ev, message }: SearchOptions): void => {
-    if (success.value) {
-      searchParams.value.q = searchParams.value.tagName
-        ? JSON.stringify({ tag_name: searchParams.value.tagName ?? '' })
-        : '{}'
-      submit()
-    } else {
-      errors.value.tagName = message
-      ev.preventDefault()
-    }
+    return success
   }
 
   const submit = (): void => {
     const { criteriaCookie } = useCookie()
 
-    if (searchParams.value.q) {
-      criteria.value = searchParams.value.q
-      criteriaCookie.value = searchParams.value.q
+    if (queryMap.value.q) {
+      criteria.value = queryMap.value.q
+      criteriaCookie.value = queryMap.value.q
       ;(options?.el as HTMLFormElement).requestSubmit()
     }
   }
