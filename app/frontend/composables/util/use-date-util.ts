ade2056f6e8a992aee4b0ca337756cd1f3090931
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-03 07:59:45 +0900

    refactor(frontend/composable): refactor util/use-date-util

--- app/frontend/composables/util/use-date-util.ts
+++ app/frontend/composables/util/use-date-util.ts
@@ -1,6 +1,6 @@
 export function useDateUtil() {
   const isValidDate = (str: string): boolean => {
-    return !isNaN(Date.parse(str))
+    return !!str.match(/\d{4}\/\d{2}\/\d{2}/) && !isNaN(Date.parse(str))
   }
 
   return { isValidDate }
