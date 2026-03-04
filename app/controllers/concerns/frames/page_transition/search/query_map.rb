51751bc3e85a1d0767d4cccca6df28b9fb1a7a52
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-05 07:29:53 +0900

    refactor(controller/concern):  Refactor frames/page_transition/search/query_map

--- app/controllers/concerns/frames/page_transition/search/query_map.rb
+++ app/controllers/concerns/frames/page_transition/search/query_map.rb
@@ -7,10 +7,10 @@ class Frames::PageTransition::Search::QueryMap
 
   def build_query
     query = {}
-    items_from = ref_items[:from]
-    items_page = ref_items[:page]
-    query[:page] = items_page if items_from.blank? && items_page.present?
-    query[:ref] = Oj.dump(ref_items) if items_from.present?
+    from = ref_items[:from]
+    page = ref_items[:page]
+    query[:page] = page if from.blank? && page.present?
+    query[:ref] = Oj.dump(ref_items) if from.present?
     query
   end
 
