56b53cfb6d8c7996690c68a1fe2d8daedfaf1801
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-08 14:05:25 +0900

    refactor(frontend): Refactor file structure

--- app/frontend/interfaces/index.ts
+++ app/frontend/interfaces/index.ts
@@ -1,6 +1,6 @@
 export type { BackendErrorInfo, Flash } from '@voyage/vue/interfaces'
 export type { ErrorsResource, BackendErrorResource } from '@voyage/vue/interfaces'
 
-export type { Comment, User } from './entity'
+export type { Comment, User } from './model'
 export type { RefItems } from './query'
 export type { AccountResource, CommentResource, CommentsResource, TagsResource } from './resource'
--- app/frontend/types/index.ts
+++ app/frontend/types/index.ts
@@ -1,5 +1,5 @@
 export type { ErrorMessages } from '@voyage/vue/types'
 
-export type { CommentErrorProperty } from './entity/error'
+export type { CommentErrorProperty } from './model/error'
 
 export type { Criteria } from './criiteria'
