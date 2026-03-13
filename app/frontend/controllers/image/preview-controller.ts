bc88e441e39d2dea3f696faf8408b2ecb262d0e3
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-13 22:09:48 +0900

    refactor(frontend/controller): Refactor image/preview-controller

--- app/frontend/controllers/image/preview-controller.ts
+++ app/frontend/controllers/image/preview-controller.ts
@@ -13,16 +13,16 @@ export default class PreviewController extends ApplicationController {
   declare readonly hasImageTarget: boolean
 
   contentElement: HTMLElement | null = null
-  previewElement: HTMLImageElement | null = null
+  imageElement: HTMLImageElement | null = null
 
   connect(): void {
     if (this.hasContentTarget) this.contentElement = this.contentTarget
-    if (this.hasImageTarget) this.previewElement = this.imageTarget
+    if (this.hasImageTarget) this.imageElement = this.imageTarget
   }
 
   upload(evt: Event): void {
     // (.file_fieldからデータを取得して変数fileに代入します)
     const file: File | null = (evt.target as HTMLInputElement).files?.item(0) ?? null
-    useImagePreview({ file, contentEl: this.contentElement, previewEl: this.previewElement })
+    useImagePreview({ file, contentEl: this.contentElement, previewEl: this.imageElement })
   }
 }
