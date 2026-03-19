f340296fd19bdc1cf64ea163eef5b5ec53631582
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-19 09:06:08 +0900

    refactor(controller/concern): Refactor users/page_transition/ref

--- app/controllers/concerns/users/page_transition/ref.rb
+++ app/controllers/concerns/users/page_transition/ref.rb
@@ -10,15 +10,6 @@ module Users::PageTransition::Ref
     @query_map ||= QueryMap.build(ref_items: ref_items_for__frame)
   end
 
-  def back_to_path
-    @back_to_path ||= prev_url
-    # BackToPath.build(
-    #  ref_items: Json::Util.to_hash(ref),
-    #  root_path: root_path(query_map_for_search),
-    #  prev_url:, page:, action_name:
-    # )
-  end
-
   def ref_items_for__frame
     @ref_items_for_frame ||= RefItems.build(ref_items: Json::Util.to_hash(ref))
   end
