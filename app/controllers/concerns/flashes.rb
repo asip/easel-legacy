1483c187f1478aab69d024f19e0da7a853a1790d
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-29 19:51:30 +0900

    refactor(controller/concern): Refactor flashes

--- app/controllers/concerns/flashes.rb
+++ app/controllers/concerns/flashes.rb
@@ -6,17 +6,11 @@ module Flashes
 
   included do
     helper_method :flashes
-
-    before_action :init_flashes
   end
 
   protected
 
   def flashes
-    @flashes
-  end
-
-  def init_flashes
-    @flashes = {}
+    @flashes ||= {}
   end
 end
