d11ff135e1b6cbc41a0eb9e841a15bd76219b946
Yasumasa Ashida <ys.ashida@gmail.com>
2026-04-08 15:47:27 +0900

    refactor(config): Refactor initializers/locale.rb

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-Rails.application.config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}").to_s]
+Rails.application.config.i18n do |i18n|
+  i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}").to_s]
 
-Rails.application.config.i18n.available_locales = [ :en, :ja ]
-Rails.application.config.i18n.enforce_available_locales = false
-Rails.application.config.i18n.default_locale = :en
-Rails.application.config.i18n.fallbacks = true
+  i18n.available_locales = [ :en, :ja ]
+  i18n.enforce_available_locales = false
+  i18n.default_locale = :en
+  i18n.fallbacks = true
+end
 
 # I18n.available_locales = %i[en ja]
 # I18n.enforce_available_locales = false
