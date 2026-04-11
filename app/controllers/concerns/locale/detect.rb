89b71d6bc68d1e1a92f9c9ddc45a956450f844ef
Yasumasa Ashida <ys.ashida@gmail.com>
2026-04-12 07:38:56 +0900

    fix(controller/concern): Fix  locale/detect

--- app/controllers/concerns/locale/detect.rb
+++ app/controllers/concerns/locale/detect.rb
@@ -11,9 +11,9 @@ module Locale::Detect
   protected
 
   def switch_locale
-    # logger.debug I18n.locale
-    browser_locale_code = I18n.locale.to_s&.split(/-/)&.first
-    locale_code = I18n.available_locales.include?(browser_locale_code.to_sym) ? browser_locale_code : Rails.application.config.i18n.default_locale
+    browser_locale_code = request.env["HTTP_ACCEPT_LANGUAGE"]&.scan(/^[a-z]{2}/)&.first || ""
+    # logger.debug browser_locale_code
+    locale_code = I18n.available_locales.include?(browser_locale_code.to_sym) ? browser_locale_code : I18n.default_locale
     # logger.debug "* Locale set to '#{locale_code}'"
     I18n.locale = locale_code.to_sym
   end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -3,13 +3,11 @@
 Rails.application.config.i18n do |i18n|
   i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}").to_s]
 
-  i18n.available_locales = [ :en, :ja ]
   i18n.enforce_available_locales = false
   i18n.default_locale = :en
   i18n.fallbacks = true
 end
 
-# I18n.available_locales = %i[en ja]
 # I18n.enforce_available_locales = false
 # I18n.default_locale = :en
 # I18n.fallbacks = true
