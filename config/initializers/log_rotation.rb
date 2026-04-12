217a65c317b0b20b9c91e102fe409528ac8918d5
Yasumasa Ashida <ys.ashida@gmail.com>
2026-04-12 10:21:22 +0900

    refactor(config): Refactor environments/*

---
+++ config/initializers/log_rotation.rb
@@ -0,0 +1,8 @@
+
+# frozen_string_literal: true
+
+if Rails.env.development?
+  Rails.application.config.logger = Logger.new("log/development.log", 10, 5 * 1024 * 1024)
+elsif Rails.env.test?
+  Rails.application.config.logger = Logger.new("log/test.log", 10, 5 * 1024 * 1024)
+end
