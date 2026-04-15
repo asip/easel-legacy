4a08c435706a0e3761f635af1998add54a2649aa
Yasumasa Ashida <ys.ashida@gmail.com>
2026-04-16 05:48:18 +0900

    refactor(controller/concern): Refactor error_renderable

--- app/controllers/concerns/error_renderable.rb
+++ app/controllers/concerns/error_renderable.rb
@@ -6,10 +6,6 @@ module ErrorRenderable
 
   protected
 
-  def render_error_stream
-    render layout: false, content_type: "text/vnd.turbo-stream.html", status: :unprocessable_content
-  end
-
   def render_errors(resource:)
     # puts errors.to_hash(true)
     flashes[:alert] = resource.full_error_messages unless resource.errors.empty?
@@ -19,5 +15,11 @@ module ErrorRenderable
   def render_login_errors(resource:)
     flashes[:alert] = resource.full_error_messages_on_login unless resource.errors.empty?
     render_error_stream
+  end
+
+  private
+
+  def render_error_stream
+    render layout: false, content_type: "text/vnd.turbo-stream.html", status: :unprocessable_content
   end
 end
