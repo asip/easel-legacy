71c255b2a1d0679a89d40e4247e1289720305940
Yasumasa Ashida <ys.ashida@gmail.com>
2026-05-01 10:25:00 +0900

    refactor(validator): Refactor datetime_validator

--- app/validators/datetime_validator.rb
+++ app/validators/datetime_validator.rb
@@ -8,8 +8,8 @@ class DatetimeValidator < ActiveModel::EachValidator
     begin
       DateTime.parse(value.to_s)
     rescue
-      attr_str = attribute.to_s
-      attribute = attr_str.sub("_before_type_cast", "").to_sym if attr_str.include?("_before_type_cast")
+      attr = attribute.to_s
+      attribute = attr.sub("_before_type_cast", "").to_sym if attr.include?("_before_type_cast")
       record.errors.add(options[:attribute] || attribute, options[:message] || I18n.t("validations.message.model.datetime.invalid"))
     end
   end
