ccfe05298b40f36543a51a2b69f8a6cfda3e852c
Yasumasa Ashida <ys.ashida@gmail.com>
2026-05-10 12:18:10 +0900

    refactor(component): Refactor preview/image

--- app/components/preview/image/component.rb
+++ app/components/preview/image/component.rb
@@ -6,22 +6,8 @@ module Preview
   module Image
     # Component class
     class Component < ViewComponent::Base
-      def initialize(model:)
-        @img_url = get_url(model:)
-      end
-
-      private
-
-      def get_url(model:)
-        if model.class == Frame && model.file.present?
-          img_url = model.file_proxy_url(:three)
-        elsif model.class == User && model.image.present?
-          img_url = model.image_proxy_url(:three)
-        else
-          img_url = nil
-        end
-
-        img_url
+      def initialize(url:)
+        @url = url
       end
     end
   end
