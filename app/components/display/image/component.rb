2fc3a978ca879633a9c62da93152fb42c1aaa654
Yasumasa Ashida <ys.ashida@gmail.com>
2026-05-10 11:14:56 +0900

    refactor(component): Refactor display/image

--- app/components/display/image/component.rb
+++ app/components/display/image/component.rb
@@ -7,10 +7,22 @@ module Display
     # Component class
     class Component < ViewComponent::Base
       def initialize(model:, original: false, photoswipe: false, small: false)
-        @model = model
-        @original = original
         @photoswipe = photoswipe
         @small = small
+
+        @img_url, @link_url = set_urls(model:, original:)
+      end
+
+      def set_urls(model:, original:)
+        if model.class == Frame && model.file.present?
+          img_url = model.file_proxy_url(:three)
+          link_url = original ? model.file_proxy_url(:original) : nil
+        elsif model.class == User && model.image.present?
+          img_url = model.image_url_for_view(:three)
+          link_url = original ? model.image_url_for_view(:original) : nil
+        end
+
+        return [img_url, link_url]
       end
     end
   end
