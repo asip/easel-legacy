9a565c4748c66ec89876475a8b03a51caccc0115
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-05 22:03:56 +0900

    refactor(component): Refactor frames/more/list/item

--- app/components/frames/more/list/item/component.rb
+++ app/components/frames/more/list/item/component.rb
@@ -2,7 +2,7 @@
 
 # Frames::More::List::Item::Component class
 class Frames::More::List::Item::Component < ViewComponent::Base
-  def initialize(frame:, tag: true, query_map: {})
+  def initialize(frame:, tag: true, query_map:)
     @frame = frame
     @tag = tag
     @query_map = query_map
