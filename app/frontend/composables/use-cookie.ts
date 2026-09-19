8c435c3ccfe7c8f8cddfdeea0f69cd9cfb9d588b
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-19 11:11:24 +0900

    refactor(composable): Refactor useCookie

--- app/frontend/composables/use-cookie.ts
+++ app/frontend/composables/use-cookie.ts
@@ -5,17 +5,17 @@ import { useCookies } from '@vueuse/integrations/useCookies'
 import { CookieRef } from '@/types'
 
 export const useCookie = function <T = string>(
-  attr: string,
+  name: string,
   options?: CookieSetOptions,
 ): CookieRef<T> {
-  const cookies = useCookies([attr])
+  const cookies = useCookies([name])
 
   const cookie: CookieRef<T> = computed<T, T | string>({
     get() {
-      return cookies.get<T>(attr)
+      return cookies.get<T>(name)
     },
     set(value: T | string) {
-      cookies.set(attr, value, options)
+      cookies.set(name, value, options)
     },
   })
 
