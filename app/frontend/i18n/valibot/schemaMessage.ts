7397d7ffeb329758e4b5adc1de57c720b1abac95
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-20 21:46:49 +0900

    doc(i18n): Fix valibot/schemaMessage

--- app/frontend/i18n/valibot/schemaMessage.ts
+++ app/frontend/i18n/valibot/schemaMessage.ts
@@ -5,10 +5,6 @@ import { useI18nGlobal } from '@vesperjs/vue'
 const { t, locale } = useI18nGlobal()
 
 export const schemaMessage = () => {
-  /*
-    v.setSchemaMessage((issue) => `Invalid type: Please enter as type ${issue.expected ?? ''}, not type ${issue.received}`, 'en')
-    v.setSchemaMessage((issue) => `無効な型です：${issue.received}型ではなく${issue.expected ?? ''}型で入力してください`, 'ja')
-    */
   v.setSchemaMessage(
     (issue) =>
       t('rules.schemaMessage', {
