11cb2e02bf8e577cac63afcf5deeeda5c91da080
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-22 21:39:22 +0900

    refactor(composable): Refactor useCookieValueObject

--- app/frontend/composables/use-cookie-value-object.ts
+++ app/frontend/composables/use-cookie-value-object.ts
@@ -18,7 +18,7 @@ export const useCookieValueObject = function <T extends object>(
         valueRef.value = cookie.value ? (JSON.parse(cookie.value) as T) : null
         return valueRef.value
       },
-      set(value: T | string | undefined) {
+      set(value: T | string | null | undefined) {
         if (typeof value == 'string') {
           valueRef.value = value ? (JSON.parse(value) as T) : null
           cookie.value = value
