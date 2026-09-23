9fbc5cc544b7c68ca1c379199139cad6f51286e7
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-23 09:31:12 +0900

    refactor(composable): Refactor useCookieValueObject

--- app/frontend/composables/use-cookie-value-object.ts
+++ app/frontend/composables/use-cookie-value-object.ts
@@ -13,26 +13,26 @@ export const useCookieValueObject = function <T extends object>(
     | undefined
 
   if (deep) {
-    const coieValueRef = ref<T | null>()
+    const cookieValueRef = ref<T | null>()
 
     cookieValue = computed<T | null | undefined, T | string | null | undefined>({
       get() {
-        coieValueRef.value = cookie.value ? (JSON.parse(cookie.value) as T) : null
-        return coieValueRef.value
+        cookieValueRef.value = cookie.value ? (JSON.parse(cookie.value) as T) : null
+        return cookieValueRef.value
       },
       set(value: T | string | null | undefined) {
         if (typeof value == 'string') {
-          coieValueRef.value = value ? (JSON.parse(value) as T) : null
+          cookieValueRef.value = value ? (JSON.parse(value) as T) : null
           cookie.value = value
         } else {
-          coieValueRef.value = value
+          cookieValueRef.value = value
           cookie.value = JSON.stringify(value ?? {})
         }
       },
     })
 
-    watch(coieValueRef, () => {
-      if (cookieValue) cookieValue.value = coieValueRef.value
+    watch(cookieValueRef, () => {
+      if (cookieValue) cookieValue.value = cookieValueRef.value
     })
   } else {
     cookieValue = computed<T | null | undefined, T | string | null | undefined>({
