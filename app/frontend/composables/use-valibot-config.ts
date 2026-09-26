154350ed9989537d606a253fe77504953f2ec6e4
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-26 23:38:46 +0900

    refactor(composable): Refactor useValibotConfig

--- app/frontend/composables/use-valibot-config.ts
+++ app/frontend/composables/use-valibot-config.ts
@@ -3,9 +3,8 @@ import { useI18nGlobal } from '@vesperjs/vue'
 import { useValibotI18n } from '@/composables'
 import { maxLength } from '@/i18n/valibot/actions'
 
-const { locale } = useI18nGlobal()
-
 export const useValibotConfig = () => {
+  const { locale } = useI18nGlobal()
   const { initValibotI18n, setup } = useValibotI18n({ lang: locale.value })
 
   setup(() => {
