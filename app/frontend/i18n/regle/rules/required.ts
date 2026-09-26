c5aca1201e02ea48095ba7419c566f8821852de8
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-26 23:40:16 +0900

    refactor(i18n): Refactor regle/rules/required

--- app/frontend/i18n/regle/rules/required.ts
+++ app/frontend/i18n/regle/rules/required.ts
@@ -1,8 +1,7 @@
 import * as r from '@regle/rules'
 import { useI18nGlobal } from '@vesperjs/vue'
 
-const { t } = useI18nGlobal()
-
 export const required = function () {
+  const { t } = useI18nGlobal()
   return r.withMessage(r.required, () => t('regle.rules.required'))
 }
